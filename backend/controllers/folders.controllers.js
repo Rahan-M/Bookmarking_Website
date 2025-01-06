867b667821135d6ad19642cec70caa6a74a4cf6b
Rahan-M <rahan01729@gmail.com>
2025-01-06 07:14:36 +0530

    Added feature to delete a folder and all bookmarks in it and the feature to rename a folder (the latter is only partially done)

--- backend/controllers/folders.controllers.js
+++ backend/controllers/folders.controllers.js
@@ -1,6 +1,4 @@
-import { Folder } from "../models/models.js";
-import mongoose from "mongoose";
-
+import { BookMark, Folder } from "../models/models.js";
 
 const getAllfolders = async (req, res) => {
   const folders = await Folder.find({});
@@ -13,7 +11,19 @@ const getAllfolders = async (req, res) => {
   res.status(200).json({ success: true, data: folders });
 };
 
-
+const renameFolder = async (req, res) => {
+  const {id} = req.params;
+  const { newName } = req.body;
+  const updationJSON={name:newName};
+  const requiredFolder = await Folder.findById(id); // Returns document as it was before updation
+  const oldName = requiredFolder.name;
+  const updatedFolder = await Folder.findByIdAndUpdate(id, updationJSON, { new: true });
+  await BookMark.updateMany(
+    { folder: oldName },
+    { $set: { folder: newName } }
+  );
+  res.status(200).json({ success: true, data: updatedFolder });
+};
 
 const deleteFolder = async (req, res) => {
   const { id } = req.params;
@@ -21,17 +31,22 @@ const deleteFolder = async (req, res) => {
     return res.status(400).json({ success: false, msg: "Please Proide Id" });
   }
   const deletedFolder = await Folder.findByIdAndDelete(id);
+  const folderName = deletedFolder.name;
+  await BookMark.deleteMany({ folder: folderName });
   res.status(200).json({ success: true, data: deletedFolder });
 };
 
-const deleteAllFolders=async (req,res)=>{
-  try{
+const deleteAllFolders = async (req, res) => {
+  try {
     await Folder.deleteMany({});
-    return res.status(200).json({success:true, msg:"Collection Cleared Succesfully"})
+    return res
+      .status(200)
+      .json({ success: true, msg: "Collection Cleared Succesfully" });
+  } catch {
+    return res
+      .status(500)
+      .json({ success: false, msg: "Some Error occured from our side" });
   }
-  catch{
-    return res.status(500).json({success:false,msg:"Some Error occured from our side"});
-  }
-}
+};
 
-export { getAllfolders, deleteFolder ,deleteAllFolders};
+export { getAllfolders, deleteFolder, deleteAllFolders, renameFolder };
--- backend/routes/folders.routes.js
+++ backend/routes/folders.routes.js
@@ -1,9 +1,9 @@
-import { getAllfolders, deleteFolder, deleteAllFolders } from "../controllers/folders.controllers.js";
+import { getAllfolders, deleteFolder, deleteAllFolders, renameFolder } from "../controllers/folders.controllers.js";
 import express from 'express';
 
 const router=express.Router();
 router.route('/').get(getAllfolders);
 router.route("/deleteAll").delete(deleteAllFolders);
-router.route('/:id').delete(deleteFolder);
+router.route('/:id').delete(deleteFolder).put(renameFolder);
 
 export default router;
