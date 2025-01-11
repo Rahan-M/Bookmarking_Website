61564467d37dd2e1b1d31c6c83565d10a990e3c6
Rahan-M <rahan01729@gmail.com>
2025-01-11 10:04:02 +0530

    Designed navbar first draft

--- frontend/src/components/navbar.tsx
+++ frontend/src/components/navbar.tsx
@@ -1,9 +1,18 @@
-import React from 'react'
-
-const navbar = () => {
+import React from "react";
+import logo from "../assets/logo.png";
+import { FaUserCircle } from "react-icons/fa";
+import { GiHamburgerMenu } from "react-icons/gi";
+const Navbar = () => {
   return (
-    <div className='red'>navbar</div>
-  )
-}
+    <div className="bg-gradient-to-br from-cyan-500 to-blue-500 flex justify-between h-14 items-center">
+      <GiHamburgerMenu color="white" className="pl-5 text-6xl" />
+      <div className="flex items-center">
+        <img src={logo} className="h-[6rem] pt-1"></img>
+        <h1 className="font-bold text-4xl pb-2 text-white">BookMarq</h1>
+      </div>
+      <FaUserCircle className="pr-5 text-6xl" color="white" />
+    </div>
+  );
+};
 
-export default navbar
+export default Navbar;
